06098507e751daa2636cfb5c898b1052e0ba1e2e
Karan Kumar <karan9302451907@gmail.com>
2026-07-19 01:22:02 +0530

    Remove test scripts

--- server/test_db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://karankumar:LwG8Y7xXqK7iH40X@cluster0.o5hck.mongodb.net/lpu_assistant?retryWrites=true&w=majority&appName=Cluster0')
-.then(async () => {
-  const docs = await mongoose.connection.db.collection('documents').find({}).sort({createdAt: -1}).limit(5).toArray();
-  docs.forEach(d => console.log(d.title));
-  mongoose.disconnect();
-});
